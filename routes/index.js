634e98f6bb351693ea0918668f21647a3a769aa3
SingleNumber <yixiaomeng@gmail.com>
2017-10-21 12:13:58 -0700

    update

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var nconf = require('../libs/common/nconf');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'viashots' });
+  res.render('index', { title: 'viashots production' });
 });
 
 module.exports = router;
